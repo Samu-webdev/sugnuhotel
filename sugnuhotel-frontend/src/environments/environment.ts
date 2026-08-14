be49a2568d0a88b8af4d201d1dfb9ef08a73efec
Samu-webdev <leumas.agr29@gmail.com>
2026-08-14 15:56:45 +0000

    Prepare frontend for deployment

--- sugnuhotel-frontend/src/environments/environment.ts
+++ sugnuhotel-frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
-// URL de base de l'API Laravel. En développement, php artisan serve écoute sur :8000.
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api',
+  production: true,
+  apiUrl: 'https://sugnuhotel.onrender.com/api'
 };
